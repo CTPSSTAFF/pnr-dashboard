69a0ef69edacdb94be117adc8b6ec5a4f890ab8c
Ben Krepp <bkrepp@ctps.org>
2020-03-18 11:17:19 -0400

    Update pnrDashboard.js

--- pnrDashboard.js
+++ pnrDashboard.js
@@ -1,5 +1,7 @@
-var map = new ol.Map({  target: 'map',
+function initialize() {
+    var map = new ol.Map({  target: 'map',
                         layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }) ],
                         view:   new ol.View({ center: ol.proj.fromLonLat([-71.0589, 42.3601]),
                                               zoom: 12 })
-                    });
+                        });
+}
