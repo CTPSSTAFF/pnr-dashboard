56a74ca2aeac8d8063034bdda11684a7bb4bbcd8
Ben Krepp <bkrepp@ctps.org>
2020-03-18 17:10:18 -0400

    Minor formatting tweaks.

--- pnrDashboard.js
+++ pnrDashboard.js
@@ -1,7 +1,7 @@
 function initialize() {
     var map = new ol.Map({  target: 'map',
-                        layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }) ],
-                        view:   new ol.View({ center: ol.proj.fromLonLat([-71.0589, 42.3601]),
+                            layers: [ new ol.layer.Tile({ source: new ol.source.OSM() }) ],
+                            view:   new ol.View({ center: ol.proj.fromLonLat([-71.0589, 42.3601]),
                                               zoom: 12 })
                         });
 }
